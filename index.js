dd8e3905531db81a427710121a5eb2dab45821e2
dineshkumar-mb <166787277+dineshkumar-mb@users.noreply.github.com>
2024-07-18 12:06:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { UserRouter } from './routes/user.js'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["https://chipper-marigold-f7720c.netlify.app"],
+    origin: [" https://heroic-cactus-c569b6.netlify.app"],
     credentials: true
 }))
 app.use(cookieParser())
